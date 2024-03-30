0c46a87a26280d4d2337f0383badc6e6b80d3827
Sam Dupras <sam.dupras@gmail.com>
2024-03-30 14:14:41 -0400

    fixup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
+const path = require('path');
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs')
+    }
+  }
 })
